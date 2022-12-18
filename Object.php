876f9b126249ba24070a2d88d8a32568ad851e68
reimiii <imiia75775@gmail.com>
2022-12-18 21:08:24 +0700

    Test error

--- Object.php
+++ Object.php
@@ -12,3 +12,7 @@ var_dump($user);
 echo "Name : $user->name" . PHP_EOL;
 echo "Address : $user->address" . PHP_EOL;
 echo "Country : $user->country" . PHP_EOL;
+
+// test error
+//$user2 = new User();
+//$user2->name = [];
