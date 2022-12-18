17b2a938c745e06667d349e51c70b540e48c1213
reimiii <imiia75775@gmail.com>
2022-12-18 22:22:35 +0700

    clean code

--- data/User.php
+++ data/User.php
@@ -2,20 +2,20 @@
 
 class User
 {
-    const AUTHOR = 'REIMII';
+    public const AUTHOR = 'REIMII';
 
     public string $name;
     public ?string $address = null;
     // Default value
     public string $country = "Indonesia";
 
-    function __construct(string $name, ?string $address)
+    public function __construct(string $name, ?string $address)
     {
         $this->name = $name;
         $this->address = $address;
     }
 
-    function sayHello(?string $name): void
+    public function sayHello(?string $name): void
     {
         if (is_null($name)) {
             echo "Hi, my name is $this->name".PHP_EOL;
@@ -24,7 +24,7 @@ class User
         }
     }
 
-    function info(): void
+    public function info(): void
     {
         echo 'Author : '.self::AUTHOR.PHP_EOL;
     }
