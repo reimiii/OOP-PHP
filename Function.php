c686da2ea31e32e1f82542a1ecf97c6ee6b361d2
reimiii <imiia75775@gmail.com>
2022-12-18 21:18:57 +0700

    Function

---
+++ Function.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "data/User.php";
+
+$user = new User();
+
+$user->sayHello("Hilmi");
--- data/User.php
+++ data/User.php
@@ -4,7 +4,11 @@ class User
 {
     public string $name;
     public ?string $address = null;
-
     // Default value
     public string $country = "Indonesia";
+
+    function sayHello(string $name)
+    {
+        echo "Hello $name" . PHP_EOL;
+    }
 }
